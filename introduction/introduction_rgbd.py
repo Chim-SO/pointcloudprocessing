d37b2b8a0ad50c92195db1005228cf2e9b3394b6
Chim SO <chimso1994@gmail.com>
2022-08-04 10:13:26 +0200

    Rename variables.

--- introduction/introduction_rgbd.py
+++ introduction/introduction_rgbd.py
@@ -2,17 +2,16 @@ import open3d as o3d
 
 if __name__ == '__main__':
     # read the color and the depth image:
-    color_raw = o3d.io.read_image("../data/rgb.jpg")
-    depth_raw = o3d.io.read_image("../data/depth.png")
+    color_im = o3d.io.read_image("../data/rgb.jpg")
+    depth_im = o3d.io.read_image("../data/depth.png")
 
     # create an rgbd image object:
     rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
-        color_raw, depth_raw, convert_rgb_to_intensity=False)
+        color_im, depth_im, convert_rgb_to_intensity=False)
     # use the rgbd image to create point cloud:
     pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
         rgbd_image,
         o3d.camera.PinholeCameraIntrinsic(
             o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
-
     # visualize:
     o3d.visualization.draw_geometries([pcd])
