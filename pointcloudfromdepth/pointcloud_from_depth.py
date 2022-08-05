713b7426cc518ec1723dd0ce37454c5044754bec
Chim SO <chimso1994@gmail.com>
2022-08-05 15:13:39 +0200

    Running time optimization.

--- pointcloudfromdepth/pointcloud_from_depth.py
+++ pointcloudfromdepth/pointcloud_from_depth.py
@@ -3,7 +3,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import open3d as o3d
 
+
+def compute_pcd_nested_loops(depth_im):
+    """
+    Compute the point cloud using nested loops.
+    This function is written for demonstration purposes.
+    It takes time.
+    :param depth_im: a depth image
+    :return: the computed point cloud
+    """
+    height, width = depth_im.shape
+    pcd = []
+    for i in range(height):
+        for j in range(width):
+            z = depth_image[i][j]
+            x = (j - CX_DEPTH) * depth_im[i][j] / FX_DEPTH
+            y = (i - CY_DEPTH) * depth_im[i][j] / FY_DEPTH
+            pcd.append([x, y, z])
+    return pcd
+
+
+def compute_pcd_vectorization(depth_im):
+    """
+    This function use victorization operations to optimize time.
+    We can do better by computing constants first.
+    Please see the main function.
+    :param depth_im: a depth image
+    :return: the computed point cloud
+    """
+    # get depth resolution:
+    height, width = depth_im.shape
+    # compute indices:
+    ii = np.tile(range(width), height)
+    jj = np.repeat(range(height), width)
+    # rechape depth image
+    z = depth_im.reshape(height * width)
+    # compute pcd:
+    pcd = np.array([(ii - CX_DEPTH) * z / FX_DEPTH,
+                    (jj - CY_DEPTH) * z / FY_DEPTH,
+                    z]).transpose()
+    return pcd
+
+
 if __name__ == '__main__':
+    print(np.ndindex(480, 640))
     # Camera parameters:
     FX_DEPTH = 5.8262448167737955e+02
     FY_DEPTH = 5.8269103270988637e+02
@@ -31,20 +74,24 @@ if __name__ == '__main__':
     axs[1].set_title('Depth grayscale image')
     plt.show()
 
-    # compute point cloud:
-    scale = 1  # scale = 1000 if want to convert it to meters
+    # get depth image resolution:
     height, width = depth_image.shape
-    pcd = []
-    for i in range(height):
-        for j in range(width):
-            z = depth_image[i][j] / scale
-            x = (j - CX_DEPTH) * depth_image[i][j] / FX_DEPTH
-            y = (i - CY_DEPTH) * depth_image[i][j] / FY_DEPTH
-            pcd.append([x, y, z])
+    # compute indices:
+    ii = np.tile(range(width), height)
+    jj = np.repeat(range(height), width)
+    # Compute constants:
+    xx = (jj - CX_DEPTH) / FX_DEPTH
+    yy = (ii - CY_DEPTH) / FY_DEPTH
+    # transform depth image to vector of z:
+    length = height * width
+    z = depth_image.reshape(height * width)
+    # compute point cloud
+    pcd = np.array([xx * z, yy * z, z]).transpose()
 
     # visualization:
     # Convert to Open3D.PointCLoud:
     pcd_o3d = o3d.geometry.PointCloud()  # create point cloud object
     pcd_o3d.points = o3d.utility.Vector3dVector(pcd)  # set pcd_np as the point cloud points
+
     # Visualize:
     o3d.visualization.draw_geometries([pcd_o3d])
