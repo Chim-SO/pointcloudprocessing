d6c72efc26a1a8a50b83a7fff0a25e9b8f6204e1
Chim SO <chimso1994@gmail.com>
2022-08-08 13:45:35 +0200

    Replace height*width with length l86.

--- pointcloudfromdepth/pointcloud_from_depth.py
+++ pointcloudfromdepth/pointcloud_from_depth.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     yy = (ii - CY_DEPTH) / FY_DEPTH
     # transform depth image to vector of z:
     length = height * width
-    z = depth_image.reshape(height * width)
+    z = depth_image.reshape(length)
     # compute point cloud
     pcd = np.dstack((xx * z, yy * z, z)).reshape((length, 3))
 
