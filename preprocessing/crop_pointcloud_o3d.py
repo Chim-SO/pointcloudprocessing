e4ef42de6fe021133b7d11d462f734af7a69d3d8
Chim SO <chimso1994@gmail.com>
2022-09-24 10:41:11 +0200

    Crop point cloud using the method crop.

--- preprocessing/crop_pointcloud_o3d.py
+++ preprocessing/crop_pointcloud_o3d.py
@@ -1,16 +1,20 @@
+import numpy as np
 import open3d as o3d
+import math
+import itertools
 
 if __name__ == '__main__':
-    # Download data from Open3D repo:
-    demo_crop_data = o3d.data.DemoCropPointCloud()
+    # Read point cloud:
+    pcd = o3d.io.read_point_cloud("../data/depth_2_pcd.ply")
 
-    # Import point cloud and display it:
-    pcd = o3d.io.read_point_cloud(demo_crop_data.point_cloud_path)
-    o3d.visualization.draw_geometries([pcd])
+    # Create bounding box:
+    bounds = [[-math.inf, math.inf], [-math.inf, math.inf], [0.8, 2]]  # set the bounds
+    bounding_box_points = list(itertools.product(*bounds))  # create limit points
+    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
+        o3d.utility.Vector3dVector(bounding_box_points))  # create bounding box object
 
-    # Read from a json file the polygon volume to be selected:
-    polygon_volume = o3d.visualization.read_selection_polygon_volume(demo_crop_data.cropped_json_path)
+    # Crop the point cloud using the bounding box:
+    pcd_croped = pcd.crop(bounding_box)
 
-    # Crop the point cloud to get the points that are inside the polygon and display it:
-    pcd_cropped = polygon_volume.crop_point_cloud(pcd)
-    o3d.visualization.draw_geometries([pcd_cropped])
+    # Display the cropped point cloud:
+    o3d.visualization.draw_geometries([pcd_croped])
