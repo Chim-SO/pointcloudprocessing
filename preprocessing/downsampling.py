e649e70c16a9bba80dc2d5b9b9d2c9a7d3ba7ae5
Chim SO <chimso1994@gmail.com>
2022-09-28 11:23:33 +0200

    Point cloud down-sampling.

---
+++ preprocessing/downsampling.py
@@ -0,0 +1,36 @@
+import open3d as o3d
+import numpy as np
+
+if __name__ == '__main__':
+    # Read point cloud:
+    pcd = o3d.io.read_point_cloud("../data/depth_2_pcd.ply")
+
+    # Random down-sampling:
+    random_pcd = pcd.random_down_sample(sampling_ratio=0.005)
+
+    # Uniform down-sampling:
+    uniform_pcd = pcd.uniform_down_sample(every_k_points=200)
+
+    # Voxel down-sampling:
+    voxel_pcd = pcd.voxel_down_sample(voxel_size=0.4)
+
+    # Translating point clouds and updating colors to display:
+    points = np.asarray(random_pcd.points)
+    points += [-3, 3, 0]
+    random_pcd.points = o3d.utility.Vector3dVector(points)
+    random_pcd.paint_uniform_color([0, 1., 0])
+
+    points = np.asarray(uniform_pcd.points)
+    points += [0, 3, 0]
+    uniform_pcd.points = o3d.utility.Vector3dVector(points)
+    uniform_pcd.paint_uniform_color([0, 0., 1.])
+
+    points = np.asarray(voxel_pcd.points)
+    points += [3, 3, 0]
+    voxel_pcd.points = o3d.utility.Vector3dVector(points)
+    voxel_pcd.paint_uniform_color([1., 0., 1.])
+
+    # Display:
+    o3d.visualization.draw_geometries([pcd, random_pcd, uniform_pcd, voxel_pcd])
+
+
